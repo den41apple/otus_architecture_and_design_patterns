7f1d11bc551c1c2270ccb7d4d80db2a9707b6f9d
Сильных Андрей Сергеевич <assilnykh@domclick.ru>
2025-07-27 18:02:43 +0300

    main

---
+++ tests/space_battle/test_move.py
@@ -0,0 +1,65 @@
+import pytest
+
+from homeworks.space_battle.actions import Move
+from homeworks.space_battle.adapters import MovingObjectAdapter
+from homeworks.space_battle.models import Point, Angle
+from homeworks.space_battle.uobject import UObject
+
+
+def test_move_success():
+    """
+    Проверяет, что для объекта, находящегося в точке (12, 5) и движущегося со скоростью 7 вдоль оси X, движение меняет положение на (19, 5)
+    """
+    ship = UObject()
+    ship.set_property('location', Point(12, 5))
+    ship.set_property('angle', Angle(0))
+    ship.set_property('velocity', 7)
+    adapter = MovingObjectAdapter(ship)
+    move = Move(adapter)
+    move.execute()
+    assert ship.get_property('location') == Point(19, 5)
+
+
+def test_move_no_location():
+    """
+    Проверяет, что попытка сдвинуть объект, у которого невозможно прочитать положение в пространстве, приводит к ошибке
+    """
+    ship = UObject()
+    ship.set_property('angle', Angle(0))
+    ship.set_property('velocity', 3)
+    adapter = MovingObjectAdapter(ship)
+    move = Move(adapter)
+    with pytest.raises(ValueError):
+        move.execute()
+
+
+def test_move_no_velocity():
+    """
+    Проверяет, что попытка сдвинуть объект, у которого невозможно прочитать значение мгновенной скорости, приводит к ошибке
+    """
+    ship = UObject()
+    ship.set_property('location', Point(1, 1))
+    ship.set_property('angle', Angle(0))
+    adapter = MovingObjectAdapter(ship)
+    move = Move(adapter)
+    with pytest.raises(ValueError):
+        move.execute()
+
+
+def test_move_set_location_error(monkeypatch):
+    """
+    Проверяет, что попытка сдвинуть объект, у которого невозможно изменить положение в пространстве, приводит к ошибке
+    """
+    ship = UObject()
+    ship.set_property('location', Point(1, 1))
+    ship.set_property('angle', Angle(0))
+    ship.set_property('velocity', 1)
+    adapter = MovingObjectAdapter(ship)
+    move = Move(adapter)
+
+    def broken_set_location(new_value):
+        raise Exception("fail")
+
+    adapter.set_location = broken_set_location
+    with pytest.raises(Exception):
+        move.execute()
---
+++ tests/space_battle/test_rotate.py
@@ -0,0 +1,29 @@
+import pytest
+
+from homeworks.space_battle.actions import Rotate
+from homeworks.space_battle.adapters import RotatableObjectAdapter
+from homeworks.space_battle.models import Angle
+from homeworks.space_battle.uobject import UObject
+
+
+def test_rotate_success():
+    """
+    Проверяет, что поворот объекта вокруг оси работает корректно: к текущему углу прибавляется дельта
+    """
+    ship = UObject()
+    ship.set_property('angle', Angle(45))
+    adapter = RotatableObjectAdapter(ship)
+    rotate = Rotate(adapter)
+    rotate.execute(Angle(15))
+    assert ship.get_property('angle') == 60
+
+
+def test_rotate_no_angle():
+    """
+    Проверяет, что попытка повернуть объект без определённого угла приводит к ошибке
+    """
+    ship = UObject()
+    adapter = RotatableObjectAdapter(ship)
+    rotate = Rotate(adapter)
+    with pytest.raises(ValueError):
+        rotate.execute(10)
